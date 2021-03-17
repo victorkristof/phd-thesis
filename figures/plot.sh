f2faca9d1c8b7b9f3c1c9d33fe777137aa531e20
Victor Kristof <victor.kristof@epfl.ch>
2021-03-17 11:54:01 +0100

    Improve script to plot all figures

--- figures/plot.sh
+++ figures/plot.sh
@@ -1,8 +1,24 @@
 #! /bin/sh
 
-figures=../text/figures
-for fig in "${@}"; do
+# This script plots the figures given as argument and save them in
+# the text/figures/ folder to be used in Latex.
+# If no arguments are given, it plots all figures.
+
+BASEDIR=$(dirname "$0")
+FIGDIR="$BASEDIR"/../text/figures
+
+# If there are some arguments, use them to plot the figures.
+if [ "$#" -gt 0 ]; then
+    figures="${@}"
+# If there are no arguments, plot all figures.
+else
+    figures=$(find -X $BASEDIR -maxdepth 1 -mindepth 1 -type d | cut -c 3- | xargs basename -a)
+fi
+
+for fig in $figures; do
     echo "Plotting $fig..."
-    python "$fig"/plot.py --save_as $figures/$fig.pdf
+    script="$BASEDIR"/"$fig"/plot.py
+    path=$FIGDIR/$fig.pdf
+    python $script --save_as $path
 done
 echo "Done."
