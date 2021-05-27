07183ee4f62c84aebfd8aa9eb590f45f9fae07a0
Victor Kristof <victor.kristof@epfl.ch>
2021-05-27 16:40:44 +0200

    Fix script to plot and save figures

--- figures/plot.sh
+++ figures/plot.sh
@@ -4,7 +4,10 @@
 # the text/figures/ folder to be used in Latex.
 # If no arguments are given, it plots all figures.
 
-BASEDIR=$(dirname "$0")
+# Get absolute path to directory of this script.
+BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# Get absolute path to directory of figures for thesis.
+FIGDIR="$(cd "$BASEDIR/../text/figures" ; pwd -P)"
 
 # If there are some arguments, use them to plot the figures.
 if [ "$#" -gt 0 ]; then
@@ -18,7 +21,7 @@ for fig in $figures; do
     echo "Plotting $fig..."
     # Change to the directory the figure.
     cd "$BASEDIR"/"$fig"
-    python plot.py --save_as $fig.pdf
+    python plot.py --save_as $FIGDIR/$fig.pdf
     cd -
 done
 echo "Done."
